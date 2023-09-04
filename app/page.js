05e4b7b509a09a44bfdb64d922d484832a6fa28a
tunny17 <tvnji17@gmail.com>
2023-09-04 21:39:11 +0100

    feat: framer motion animations

--- app/page.js
+++ app/page.js
@@ -1,17 +1,23 @@
 import { Footer, Navbar } from '../components';
-import { About, Explore, Feedback, GetStarted, Hero, Insights, WhatsNew, World } from '../sections';
+import { About, Explore, Feedback, GetStarted, Hero } from '../sections';
 
 const Page = () => (
   <div className='bg-primary-black overflow-hidden'>
     <Navbar />
     <Hero />
-    <About />
-    <Explore />
-    <GetStarted />
-    <WhatsNew />
-    <World />
-    <Insights />
-    <Feedback />
+    <div className='relative'>
+      <About />
+      <div className='gradient-03 z-0' />
+      <Explore />
+    </div>
+    <div className='relative'>
+      <GetStarted />
+      <div className='gradient-04 z-0' />
+    </div>
+    <div className='relative'>
+      <div className='gradient-04 z-0' />
+      <Feedback />
+    </div>
     <Footer />
   </div>
 );
--- sections/About.jsx
+++ sections/About.jsx
@@ -1,8 +1,47 @@
 'use client';
 
+import { motion } from 'framer-motion';
+import { TypingText } from '../components';
+
+import styles from '@/styles';
+import { fadeIn, staggerContainer } from '@/utils/motion';
+
 const About = () => (
-  <section>
-    About section
+  <section className={`${styles.paddings} relative z-10`}>
+    <div className='gradient-02 z-0' />
+    <motion.div
+      variants={staggerContainer}
+      initial='hidden'
+      whileInView='show'
+      viewport={{ once: false, amount: 0.25 }}
+      className={`${styles.innerWidth} mx-auto ${styles.flexCenter} flex-col`}>
+      <TypingText title='| About Metaversus' textStyles='text-center' />
+
+      <motion.p
+        variants={fadeIn('up', 'tween', 0.2, 1)}
+        className='mt-[8px] font-normal sm:text-[32px] text-[20px] text-center text-secondary-white'>
+        <span className='font-extrabold text-white'>Lorem ipsum</span> dolor sit
+        amet, consectetur adipisicing elit. Non modi sint ducimus aspernatur at,{' '}
+        <span className='font-extrabold text-white'>
+          {' '}
+          iure porro quo fugiat{' '}
+        </span>{' '}
+        tempora natus harum dignissimos voluptas ratione omnis vero maxime quod
+        quidem dolores?{' '}
+        <span className='font-extrabold text-white'>
+          Lorem, ipsum dolor
+        </span>{' '}
+        sit amet consectetur adipisicing elit. Quidem iste quaerat officiis quas
+        nulla aliquam eum cumque mollitia facere iure?
+      </motion.p>
+
+      <motion.img
+        variants={fadeIn('up', 'tween', 0.3, 1)}
+        src='/arrow-down.svg'
+        alt='arrow'
+        className='w-[18px] h-[28px] object-contain mt-[28px]'
+      />
+    </motion.div>
   </section>
 );
 
